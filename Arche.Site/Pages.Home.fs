ea5979d7454e50c465ce3deb71e336e6ddd7b184
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-12-31 17:45:20 +0800

    Home page changes

--- Arche.Site/Pages.Home.fs
+++ Arche.Site/Pages.Home.fs
@@ -1,8 +1,19 @@
 ﻿namespace Arche.Pages
 
 open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
 
 module Home  =
+    
+    [<JavaScript>]
+    module private Client =
+        open WebSharper.UI.Next.Client
 
-    let page = ""
+        let page() = text "Hello world"
+
+    module Static =
+
+        let html() =
+            client <@ Client.page() @>
 
--- Arche.Site/Pages.fs
+++ Arche.Site/Pages.fs
@@ -2,13 +2,14 @@
 
 open WebSharper
 open WebSharper.UI.Next
+open Arche
 open Arche.Common.Domain
 
 module All =
     let pages = [
         { Title   = "Home"
           Route   = Route.Create [ "" ]
-          Content = Doc.Empty
+          Content = Home.Static.html() 
           DisplayOption = DisplayOption.PageWithNav
           AccessOption  = AccessOption.Other }
     ]
