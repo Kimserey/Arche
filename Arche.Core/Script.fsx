9f2d407f274177c4bae9271b7c2cbaaedc026aa3
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-12-31 15:48:05 +0800

    Removed unreferenced project

--- Arche.Core/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
-open Arche.Core
-
-// Define your library scripting code here
-
