266d0aca53223929e1352cf7f0d4bba2dbe42669
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2016-01-03 00:34:43 +0800

    Changes home

--- Arche.Site/Pages.Home.fs
+++ Arche.Site/Pages.Home.fs
@@ -12,12 +12,11 @@ module Home  =
 
         let page() = divAttr [ attr.``class`` "jumbotron" ] 
                              [ h1 [ text "Hi "; iAttr [ attr.``class`` "fa fa-smile-o" ] [] ]
-                               p  [ text "Arche is a simple app built in F# with WebSharper where I demonstrate one way to structure a web app. You can find the code "
-                                    aAttr [ attr.href "https://github.com/Kimserey/Arche" ] [ text "on github" ]
-                                    text ". This web app is explained in details on my blog post "
-                                    aAttr [ attr.href "#" ] [ text "link" ]
-                                    text "." ]
+                               p  [ text "Arche is a simple app built in F# with WebSharper where I demonstrate one way to structure a web app."
+                                    br []
+                                    text "This web app is explained in details on my blog post "; aAttr [ attr.href "#" ] [ text "link" ]; text "."
+                                    br [] 
+                                    text "You can find the code "; aAttr [ attr.href "https://github.com/Kimserey/Arche" ] [ text "on github" ]; text "." ]
                                p  [ text "Get in touch on twitter "
                                     aAttr [ attr.href "https://twitter.com/Kimserey_Lam" ] [ text "@Kimserey_Lam" ]
                                     text "!" ] ]
-
