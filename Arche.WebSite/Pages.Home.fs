0cabca5353fab55b01cbf03efc02be0c6b702d04
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2016-01-03 23:00:49 +0800

    Changed blog href in home

--- Arche.WebSite/Pages.Home.fs
+++ Arche.WebSite/Pages.Home.fs
@@ -15,7 +15,7 @@ module Home  =
                                p  [ text "Arche is a simple app which demonstrates one way to build web apps in F# with WebSharper."
                                     br []
                                     text "The structure is explained in details on my blog post "
-                                    aAttr [ attr.href "#" ] [ text "link" ]
+                                    aAttr [ attr.href "http://kimsereyblog.blogspot.sg/2016/01/architecture-for-web-app-built-in-f-and.html" ] [ text "link" ]
                                     text " and the code can be found "; aAttr [ attr.href "https://github.com/Kimserey/Arche" ] [ text "on github" ]; text "." ]
                                p  [ text "Get in touch on twitter "
                                     aAttr [ attr.href "https://twitter.com/Kimserey_Lam" ] [ text "@Kimserey_Lam" ]
